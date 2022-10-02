4077f76cedd649a734a2f5cfd1ac686990aa876b
Alex <alxhendrawan@gmail.com>
2022-10-02 11:14:49 +0700

    hotfix

--- engine/app/Http/Controllers/Penjualan/CreateInvoiceController.php
+++ engine/app/Http/Controllers/Penjualan/CreateInvoiceController.php
@@ -3,13 +3,17 @@
 namespace App\Http\Controllers\Penjualan;
 
 use App\Http\Controllers\Controller;
+use App\Models\Counter;
+use App\Models\Komisi;
 use App\Models\Konsumen;
 use App\Models\SalesOrderHeader;
+use App\Models\SalesOrderLine;
 use App\Models\Stok;
 use App\Models\User;
 use Carbon\Carbon;
 use DateTime;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class CreateInvoiceController extends Controller
 {
@@ -84,6 +88,96 @@ class CreateInvoiceController extends Controller
      */
     public function store(Request $request)
     {
+        $counter = Counter::find(5);
+        $nomor = str_pad($counter->sequence_next_value, 4, 0, STR_PAD_LEFT);
+        $intnomorsales = str_pad(date("mY") . $nomor, 12, 0, STR_PAD_LEFT);
+        DB::beginTransaction();
+        $total_sales= 0;
+        //garagarapopup
+        foreach($request->subtotal as $subtotal){
+            $total_sales += $subtotal;
+        }
+        //endgaragarapopup
+
+        $header = new SalesOrderHeader();
+        $header->due_date = Carbon::parse($request->due_date)->format('Y-m-d');
+        $header->intnomorsales = $intnomorsales;
+        $header->order_date = Carbon::parse($request->order_date)->format('Y-m-d');
+        $header->payment_remain = $total_sales;
+        $header->retur = 0;
+        $header->status = "C";
+        $header->total_paid = 0;
+        $header->total_sales = $total_sales;
+        $header->customer_id = $request->customer_id;
+        $header->supir = $request->supir;
+        $header->kenek = $request->kenek;
+        $header->save();
+        $counter->sequence_next_value += 1;
+        $counter->save();
+        //preprare line
+        $modal = 0;
+        $count_line = count($request->item_stock_id);
+        $komisipersen = 0.8;
+        for ($i = 0; $i < $count_line; $i++) {
+            $line = new SalesOrderLine();
+            if ($request->harga_satuan[$i] == 0) {
+                $line->bonus = 1;
+            } else {
+                $line->bonus = 0;
+            }
+            $stock = Stok::find($request->item_stock_id[$i]);
+
+            $line->diskon = $request->discount[$i];
+            $line->price_per_satuan_id = $request->harga_satuan[$i]; //harga jual 1 an
+            $line->sales_per_satuan_id = $request->harga_modal[$i]; //harga modal 1 an
+            $line->qty = $request->quantity[$i];
+            $line->item_stock_id = $request->item_stock_id[$i];
+            $line->sales_order_header_id = $header->id;
+            $line->save();
+
+            if ($line->stock->brand) {
+                // echo $line->stock->brand->name . "<br>";
+                $komisipersen = $line->stock->brand->komisi;
+            }
+
+            $modal += $stock->purchase_price * $line->qty;
+
+            //minus Stock
+            $stock->qty -= $request->quantity[$i];
+            $stock->save();
+        }
+
+        $header->komisi = $total_sales * ($komisipersen / 100);
+        $header->modal = $modal;
+        $header->save();
+        try {
+            $komisi = Komisi::where("sales_order_header_id", $header->id)->firstorfail();
+            $komisi->amount = $header->komisi;
+            $komisi->current_amount = 0;
+            $komisi->customer_id = $header->customer->id;
+            $komisi->sales_order_header_id = $header->id;
+            $komisi->user_id = $header->customer->sales->id;
+            $komisi->percentage = $komisipersen;
+            $komisi->save();
+        } catch (\Throwable $th) {
+            $komisi = new Komisi();
+            $komisi->amount = $header->komisi;
+            $komisi->current_amount = 0;
+            $komisi->sales_order_header_id = $header->id;
+            $komisi->user_id = $header->customer->sales->id;
+            $komisi->percentage = $komisipersen;
+            $komisi->save();
+        }
+
+        DB::commit();
+        if ($request->has("prints")) {
+            $this->sendPrintPenjualan($header->id);
+        } else {
+            return response()->json("success", 200);
+        }
+
+        // $header->diskon
+
         //
     }
 
